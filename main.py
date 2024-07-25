276fac0a8a018d16dcc6be038719b4c276c200f2
kenza <kenza.azennoud>
2024-07-25 14:54:14 +0200

    visualisation des 2 avec interaction

--- main.py
+++ main.py
@@ -9,15 +9,6 @@ reader2 = vtk.vtkNrrdReader()
 reader2.SetFileName('Data/case6_gre2.nrrd')
 reader2.Update()
 
-# Ensure the whole extent is set
-information = reader.GetOutputInformation(0)
-whole_extent = reader.GetOutput().GetExtent()
-information.Set(vtk.vtkStreamingDemandDrivenPipeline.WHOLE_EXTENT(), whole_extent, 6)
-
-information2 = reader2.GetOutputInformation(0)
-whole_extent2 = reader2.GetOutput().GetExtent()
-information2.Set(vtk.vtkStreamingDemandDrivenPipeline.WHOLE_EXTENT(), whole_extent2, 6)
-
 # Calculate the center of the volume
 (xMin, xMax, yMin, yMax, zMin, zMax) = reader.GetExecutive().GetWholeExtent(reader.GetOutputInformation(0))
 (xSpacing, ySpacing, zSpacing) = reader.GetOutput().GetSpacing()
@@ -32,66 +23,27 @@ center = [x0 + xSpacing * 0.5 * (xMin + xMax),
 (x02, y02, z02) = reader2.GetOutput().GetOrigin()
 
 center2 = [x02 + xSpacing2 * 0.5 * (xMin2 + xMax2),
-          y02 + ySpacing2 * 0.5 * (yMin2 + yMax2),
-          z02 + zSpacing2 * 0.5 * (zMin2 + zMax2)]
+           y02 + ySpacing2 * 0.5 * (yMin2 + yMax2),
+           z02 + zSpacing2 * 0.5 * (zMin2 + zMax2)]
 
 # Matrices for axial, coronal, sagittal, oblique view orientations
-axial = vtk.vtkMatrix4x4()
-axial.DeepCopy((1, 0, 0, center[0],
-                0, 1, 0, center[1],
-                0, 0, 1, center[2],
-                0, 0, 0, 1))
-
 coronal = vtk.vtkMatrix4x4()
 coronal.DeepCopy((1, 0, 0, center[0],
                   0, 0, 1, center[1],
-                  0,-1, 0, center[2],
+                  0, -1, 0, center[2],
                   0, 0, 0, 1))
-
-sagittal = vtk.vtkMatrix4x4()
-sagittal.DeepCopy((0, 0,-1, center[0],
-                   1, 0, 0, center[1],
-                   0,-1, 0, center[2],
-                   0, 0, 0, 1))
-
-oblique = vtk.vtkMatrix4x4()
-oblique.DeepCopy((1, 0, 0, center[0],
-                  0, 0.866025, -0.5, center[1],
-                  0, 0.5, 0.866025, center[2],
-                  0, 0, 0, 1))
-
 
 axial2 = vtk.vtkMatrix4x4()
 axial2.DeepCopy((1, 0, 0, center2[0],
-                0, 1, 0, center2[1],
-                0, 0, 1, center2[2],
-                0, 0, 0, 1))
-
-coronal2 = vtk.vtkMatrix4x4()
-coronal2.DeepCopy((1, 0, 0, center2[0],
-                  0, 0, 1, center2[1],
-                  0,-1, 0, center2[2],
-                  0, 0, 0, 1))
-
-sagittal2 = vtk.vtkMatrix4x4()
-sagittal2.DeepCopy((0, 0,-1, center2[0],
-                   1, 0, 0, center2[1],
-                   0,-1, 0, center2[2],
-                   0, 0, 0, 1))
-
-oblique2 = vtk.vtkMatrix4x4()
-oblique2.DeepCopy((1, 0, 0, center2[0],
-                  0, 0.866025, -0.5, center2[1],
-                  0, 0.5, 0.866025, center2[2],
-                  0, 0, 0, 1))
-
-
+                 0, 1, 0, center2[1],
+                 0, 0, 1, center2[2],
+                 0, 0, 0, 1))
 
 # Extract a slice in the desired orientation
 reslice = vtk.vtkImageReslice()
 reslice.SetInputConnection(reader.GetOutputPort())
 reslice.SetOutputDimensionality(2)
-reslice.SetResliceAxes(axial)
+reslice.SetResliceAxes(coronal)
 reslice.SetInterpolationModeToLinear()
 
 reslice2 = vtk.vtkImageReslice()
@@ -100,19 +52,18 @@ reslice2.SetOutputDimensionality(2)
 reslice2.SetResliceAxes(axial2)
 reslice2.SetInterpolationModeToLinear()
 
-
 # Create a greyscale lookup table
 table = vtk.vtkLookupTable()
-table.SetRange(300, 2000) # image intensity range
-table.SetValueRange(0.0, 1.0) # from black to white
-table.SetSaturationRange(0.0, 0.0) # no color saturation
+table.SetRange(300, 2000)  # image intensity range
+table.SetValueRange(0.0, 1.0)  # from black to white
+table.SetSaturationRange(0.0, 0.0)  # no color saturation
 table.SetRampToLinear()
 table.Build()
 
 table2 = vtk.vtkLookupTable()
-table2.SetRange(300, 2000) # image intensity range
-table2.SetValueRange(0.0, 1.0) # from black to white
-table2.SetSaturationRange(0.0, 0.0) # no color saturation
+table2.SetRange(300, 2000)  # image intensity range
+table2.SetValueRange(0.0, 1.0)  # from black to white
+table2.SetSaturationRange(0.0, 0.0)  # no color saturation
 table2.SetRampToLinear()
 table2.Build()
 
@@ -140,26 +91,25 @@ renderer2.AddActor(actor2)
 
 window = vtk.vtkRenderWindow()
 window.AddRenderer(renderer)
+window.SetWindowName("gre1")
 
 window2 = vtk.vtkRenderWindow()
 window2.AddRenderer(renderer2)
-
+window2.SetWindowName("gre2")
 
 # Set up the interaction
 interactorStyle = vtk.vtkInteractorStyleImage()
 interactor = vtk.vtkRenderWindowInteractor()
 interactor.SetInteractorStyle(interactorStyle)
 window.SetInteractor(interactor)
-window.SetWindowName("gre1")
-window.Render()
 
-window2.SetWindowName("gre2")
-window2.Render()
-
+interactorStyle2 = vtk.vtkInteractorStyleImage()
+interactor2 = vtk.vtkRenderWindowInteractor()
+interactor2.SetInteractorStyle(interactorStyle2)
+window2.SetInteractor(interactor2)
 
 # Create callbacks for slicing the image
-actions = {}
-actions["Slicing"] = 0
+actions = {"Slicing": 0}
 
 def ButtonCallback(obj, event):
     if event == "LeftButtonPressEvent":
@@ -176,7 +126,7 @@ def MouseMoveCallback(obj, event):
         sliceSpacing = reslice.GetOutput().GetSpacing()[2]
         matrix = reslice.GetResliceAxes()
         # move the center point that we are slicing through
-        center = matrix.MultiplyPoint((0, 0, sliceSpacing*deltaY, 1))
+        center = matrix.MultiplyPoint((0, 0, sliceSpacing * deltaY, 1))
         matrix.SetElement(0, 3, center[0])
         matrix.SetElement(1, 3, center[1])
         matrix.SetElement(2, 3, center[2])
@@ -184,17 +134,44 @@ def MouseMoveCallback(obj, event):
     else:
         interactorStyle.OnMouseMove()
 
+def MouseMoveCallback2(obj, event):
+    (lastX, lastY) = interactor2.GetLastEventPosition()
+    (mouseX, mouseY) = interactor2.GetEventPosition()
+    if actions["Slicing"] == 1:
+        deltaY = mouseY - lastY
+        reslice2.Update()
+        sliceSpacing = reslice2.GetOutput().GetSpacing()[2]
+        matrix = reslice2.GetResliceAxes()
+        # move the center point that we are slicing through
+        center = matrix.MultiplyPoint((0, 0, sliceSpacing * deltaY, 1))
+        matrix.SetElement(0, 3, center[0])
+        matrix.SetElement(1, 3, center[1])
+        matrix.SetElement(2, 3, center[2])
+        window2.Render()
+    else:
+        interactorStyle2.OnMouseMove()
+
 interactorStyle.AddObserver("MouseMoveEvent", MouseMoveCallback)
 interactorStyle.AddObserver("LeftButtonPressEvent", ButtonCallback)
 interactorStyle.AddObserver("LeftButtonReleaseEvent", ButtonCallback)
 
-
+interactorStyle2.AddObserver("MouseMoveEvent", MouseMoveCallback2)
+interactorStyle2.AddObserver("LeftButtonPressEvent", ButtonCallback)
+interactorStyle2.AddObserver("LeftButtonReleaseEvent", ButtonCallback)
 
 # Start interaction
+window.Render()
+interactor.Initialize()
+
+window2.Render()
+interactor2.Initialize()
+
 interactor.Start()
+interactor2.Start()
 
 del renderer
 del window
 del interactor
 del renderer2
 del window2
+del interactor2
